a33a3f361551a0ad0a2d7a24fe4f536d0db33365
bisolajongbo <bisolajongbo@gmail.com>
2026-04-19 20:50:06 +0100

    Day-19 :Introduction to Bash Scripting

---
+++ day-19/practice_day_19/welcome.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo " Hello Data Engineers!"
+echo "This is your first Bash script running on $(omojesu)"
+
---
+++ day-19/practice_day_19/welcome2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Welcome to Learning Linux"
+echo  "This is your first Bash script running on $(hostname)"
+
